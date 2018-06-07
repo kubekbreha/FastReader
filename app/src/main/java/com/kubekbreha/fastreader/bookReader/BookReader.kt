af174cf7085c92981bebf5acab8f10618709d4ec
Jakub Brehuv <kubek.breha@gmail.com>
2018-06-07 13:47:37 +0200

    book reader class

---
+++ app/src/main/java/com/kubekbreha/fastreader/bookReader/BookReader.kt
@@ -0,0 +1,5 @@
+package com.kubekbreha.fastreader.bookReader
+
+interface BookReader {
+
+}
---
+++ app/src/main/java/com/kubekbreha/fastreader/bookReader/EpubBookReader.kt
@@ -0,0 +1,11 @@
+package com.kubekbreha.fastreader.bookReader
+
+class EpubBookReader : BookReader {
+
+    fun getWholeBook() : String{
+
+
+        return ""
+    }
+
+}
