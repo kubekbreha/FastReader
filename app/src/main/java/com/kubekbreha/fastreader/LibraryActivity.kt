9076d963195cb242c62956852743a57913781856
Jakub Brehuv <kubek.breha@gmail.com>
2018-06-11 16:34:32 +0200

    file picker return without failing

--- app/src/main/java/com/kubekbreha/fastreader/LibraryActivity.kt
+++ app/src/main/java/com/kubekbreha/fastreader/LibraryActivity.kt
@@ -1,89 +1,41 @@
 package com.kubekbreha.fastreader
 
-import android.Manifest
 import android.app.Activity
-import android.content.Intent
-import android.content.pm.PackageManager
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.support.v4.app.ActivityCompat
-import android.support.v4.content.ContextCompat
-import android.util.Log
-import android.widget.Toast
-import com.nbsp.materialfilepicker.MaterialFilePicker
-import com.nbsp.materialfilepicker.ui.FilePickerActivity
+import android.support.v7.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_library.*
-import android.graphics.Bitmap
+import com.nbsp.materialfilepicker.ui.FilePickerActivity
+import android.content.Intent
+import com.nbsp.materialfilepicker.MaterialFilePicker
+import java.util.regex.Pattern
 
 
 class LibraryActivity : AppCompatActivity() {
-
-    val PERMISSIONS_REQUEST_CODE = 0
-    val FILE_PICKER_REQUEST_CODE = 1
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_library)
 
-        activity_library_add_book.setOnClickListener { checkPermissionsAndOpenFilePicker() }
-
-    }
-
-
-    private fun checkPermissionsAndOpenFilePicker() {
-        val permission = Manifest.permission.READ_EXTERNAL_STORAGE
-
-        if (ContextCompat.checkSelfPermission(this, permission) != PackageManager.PERMISSION_GRANTED) {
-            if (ActivityCompat.shouldShowRequestPermissionRationale(this, permission)) {
-                showError()
-            } else {
-                ActivityCompat.requestPermissions(this, arrayOf(permission), PERMISSIONS_REQUEST_CODE)
-            }
-        } else {
-            openFilePicker()
+        activity_library_add_book.setOnClickListener{
+            MaterialFilePicker()
+                    .withActivity(this)
+                    .withRequestCode(1)
+                    .withFilter(Pattern.compile(".*\\.pdf$")) // Filtering files and directories by file name using regexp
+                    .withFilter(Pattern.compile(".*\\.epub$")) // Filtering files and directories by file name using regexp
+                    .withHiddenFiles(true) // Show hidden files and folders
+                    .start()
         }
     }
 
-    private fun showError() {
-        Toast.makeText(this, "Allow external storage reading", Toast.LENGTH_SHORT).show()
-    }
 
-    override fun onRequestPermissionsResult(requestCode: Int,
-                                            permissions: Array<String>, grantResults: IntArray) {
-        when (requestCode) {
-            PERMISSIONS_REQUEST_CODE -> {
-                if (grantResults.size > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    openFilePicker()
-                } else {
-                    showError()
-                }
-            }
-        }
-    }
-
-    private fun openFilePicker() {
-        MaterialFilePicker()
-                .withActivity(this)
-                .withRequestCode(FILE_PICKER_REQUEST_CODE)
-                .withHiddenFiles(true)
-                .start()
-    }
-
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent) {
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
 
-        if (data != null) {
-            if (requestCode == FILE_PICKER_REQUEST_CODE && resultCode == Activity.RESULT_OK) {
-                val path = data.getStringExtra(FilePickerActivity.RESULT_FILE_PATH)
-
-                if (path != null) {
-                    Log.d("Path: ", path)
-                    Toast.makeText(this, "Picked file: $path", Toast.LENGTH_LONG).show()
-                }
-            }
+        if (requestCode == 1 && resultCode == Activity.RESULT_OK) {
+            val filePath = data?.getStringExtra(FilePickerActivity.RESULT_FILE_PATH)
+            // Do anything with file
         }
     }
-
 
 }
