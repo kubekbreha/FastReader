bc2f9369ffb0f3b2d404a1ef3ea05b3599dd65d4
Jakub Brehuv <kubek.breha@gmail.com>
2018-06-13 18:55:17 +0200

    remove gestures dependencies, implemet them to reader activity

--- app/src/main/java/com/kubekbreha/fastreader/ReaderActivity.kt
+++ app/src/main/java/com/kubekbreha/fastreader/ReaderActivity.kt
@@ -3,16 +3,15 @@ package com.kubekbreha.fastreader
 import android.os.Bundle
 import android.os.Handler
 import android.support.v7.app.AppCompatActivity
-import android.util.Log
 import android.view.MotionEvent
 import android.view.ScaleGestureDetector
 import android.view.View
 import android.widget.SeekBar
 import android.widget.TextView
 import android.widget.Toast
-import com.github.nisrulz.sensey.PinchScaleDetector
-import com.github.nisrulz.sensey.Sensey
-import com.github.nisrulz.sensey.TouchTypeDetector
+import com.kubekbreha.fastreader.utils.sensey.PinchScaleDetector
+import com.kubekbreha.fastreader.utils.sensey.Sensey
+import com.kubekbreha.fastreader.utils.sensey.TouchTypeDetector
 import kotlinx.android.synthetic.main.activity_reader.*
 import java.io.*
 
@@ -37,8 +36,6 @@ class ReaderActivity : AppCompatActivity(), SeekBar.OnSeekBarChangeListener, Vie
     private var charsCount: Int = 0
 
     //gestures
-    private val LOGTAG = "TouchActivity"
-    private val DEBUG = true
     private var txtResult: TextView? = null
 
 
@@ -56,8 +53,6 @@ class ReaderActivity : AppCompatActivity(), SeekBar.OnSeekBarChangeListener, Vie
         //buttons listeners
         activity_reader_seekBar.setOnSeekBarChangeListener(this)
         activity_reader_running_imageButton.setOnClickListener(this)
-        activity_reader_plusTen_button.setOnClickListener(this)
-        activity_reader_minusTen_button.setOnClickListener(this)
         activity_reader_restart_imageButton.setOnClickListener(this)
         activity_reader_go_back.setOnClickListener(this)
 
@@ -87,7 +82,7 @@ class ReaderActivity : AppCompatActivity(), SeekBar.OnSeekBarChangeListener, Vie
 
         //start gestures detection
         startTouchTypeDetection()
-        startPinchDetection()
+//        startPinchDetection()
     }
 
     override fun onPause() {
@@ -97,7 +92,7 @@ class ReaderActivity : AppCompatActivity(), SeekBar.OnSeekBarChangeListener, Vie
         }
 
         Sensey.getInstance().stopTouchTypeDetection()
-        Sensey.getInstance().stopPinchScaleDetection()
+//        Sensey.getInstance().stopPinchScaleDetection()
     }
 
     override fun onDestroy() {
@@ -111,7 +106,7 @@ class ReaderActivity : AppCompatActivity(), SeekBar.OnSeekBarChangeListener, Vie
         super.onResume()
 
         startTouchTypeDetection()
-        startPinchDetection()
+//        startPinchDetection()
 
     }
 
@@ -135,21 +130,6 @@ class ReaderActivity : AppCompatActivity(), SeekBar.OnSeekBarChangeListener, Vie
                 Toast.makeText(this, running.toString(), Toast.LENGTH_SHORT).show()
             }
 
-            R.id.activity_reader_plusTen_button -> {
-                if (wordCounter <= testBookArray.size - 11)
-                    wordCounter += 10
-                else
-                    wordCounter = testBookArray.size - 1
-                activity_main_currentWord_textView.text = wordCounter.toString()
-            }
-
-            R.id.activity_reader_minusTen_button -> {
-                if (wordCounter >= 10)
-                    wordCounter -= 10
-                else
-                    wordCounter = 0
-                activity_main_currentWord_textView.text = wordCounter.toString()
-            }
 
             R.id.activity_reader_restart_imageButton -> {
                 wordCounter = 0
@@ -216,38 +196,25 @@ class ReaderActivity : AppCompatActivity(), SeekBar.OnSeekBarChangeListener, Vie
         return super.dispatchTouchEvent(event)
     }
 
-    private fun resetResultInView(txt: TextView) {
-        val handler = Handler()
-        handler.postDelayed({ txt.text = "resultsShowHere" }, 3000)
-    }
 
-    private fun setResultTextView(text: String) {
-        if (txtResult != null) {
-            txtResult!!.setText(text)
-            resetResultInView(txtResult!!)
-            if (DEBUG) {
-                Log.i(LOGTAG, text)
-            }
-        }
-    }
 
     private fun startPinchDetection() {
         Sensey.getInstance()
                 .startPinchScaleDetection(this@ReaderActivity, object : PinchScaleDetector.PinchScaleListener {
                     override fun onScale(scaleGestureDetector: ScaleGestureDetector, isScalingOut: Boolean) {
                         if (isScalingOut) {
-                            setResultTextView("Scaling Out")
+                            //setResultTextView("Scaling Out")
                         } else {
-                            setResultTextView("Scaling In")
+                            //setResultTextView("Scaling In")
                         }
                     }
 
                     override fun onScaleEnd(scaleGestureDetector: ScaleGestureDetector) {
-                        setResultTextView("Scaling : Stopped")
+                        //setResultTextView("Scaling : Stopped")
                     }
 
                     override fun onScaleStart(scaleGestureDetector: ScaleGestureDetector) {
-                        setResultTextView("Scaling : Started")
+                        //setResultTextView("Scaling : Started")
                     }
                 })
     }
@@ -256,45 +223,73 @@ class ReaderActivity : AppCompatActivity(), SeekBar.OnSeekBarChangeListener, Vie
         Sensey.getInstance()
                 .startTouchTypeDetection(this@ReaderActivity, object : TouchTypeDetector.TouchTypListener {
                     override fun onDoubleTap() {
-                        setResultTextView("Double Tap")
+                        //setResultTextView("Double Tap")
+                        if (running) {
+                            handler!!.removeCallbacks(handlerTask)
+                            running = false
+                            activity_reader_running_imageButton.setImageResource(R.drawable.ic_play)
+                        } else {
+                            handler!!.post(handlerTask)
+                            running = true
+                            activity_reader_running_imageButton.setImageResource(R.drawable.ic_pause)
+                        }
                     }
 
                     override fun onLongPress() {
-                        setResultTextView("Long press")
+                        //setResultTextView("Long press")
                     }
 
                     override fun onScroll(scrollDirection: Int) {
                         when (scrollDirection) {
-                            TouchTypeDetector.SCROLL_DIR_UP -> setResultTextView("Scrolling Up")
-                            TouchTypeDetector.SCROLL_DIR_DOWN -> setResultTextView("Scrolling Down")
-                            TouchTypeDetector.SCROLL_DIR_LEFT -> setResultTextView("Scrolling Left")
-                            TouchTypeDetector.SCROLL_DIR_RIGHT -> setResultTextView("Scrolling Right")
+                            //TouchTypeDetector.SCROLL_DIR_UP -> setResultTextView("Scrolling Up")
+                            //TouchTypeDetector.SCROLL_DIR_DOWN -> setResultTextView("Scrolling Down")
+                            TouchTypeDetector.SCROLL_DIR_LEFT -> {
+
+                            }
+
+                            TouchTypeDetector.SCROLL_DIR_RIGHT -> {
+
+                            }
                             else -> {
                             }
                         }// Do nothing
                     }
 
                     override fun onSingleTap() {
-                        setResultTextView("Single Tap")
+                        //setResultTextView("Single Tap")
                     }
 
                     override fun onSwipe(swipeDirection: Int) {
                         when (swipeDirection) {
-                            TouchTypeDetector.SWIPE_DIR_UP -> setResultTextView("Swipe Up")
-                            TouchTypeDetector.SWIPE_DIR_DOWN -> setResultTextView("Swipe Down")
-                            TouchTypeDetector.SWIPE_DIR_LEFT -> setResultTextView("Swipe Left")
-                            TouchTypeDetector.SWIPE_DIR_RIGHT -> setResultTextView("Swipe Right")
+                            //TouchTypeDetector.SWIPE_DIR_UP -> setResultTextView("Swipe Up")
+                            //TouchTypeDetector.SWIPE_DIR_DOWN -> setResultTextView("Swipe Down")
+                            TouchTypeDetector.SWIPE_DIR_LEFT -> {
+                                if (wordCounter <= testBookArray.size - 11)
+                                    wordCounter += 10
+                                else
+                                    wordCounter = testBookArray.size - 1
+                                activity_main_currentWord_textView.text = wordCounter.toString()
+                            }
+
+                            TouchTypeDetector.SWIPE_DIR_RIGHT -> {
+                                if (wordCounter >= 10)
+                                    wordCounter -= 10
+                                else
+                                    wordCounter = 0
+                                activity_main_currentWord_textView.text = wordCounter.toString()
+                            }
+
                             else -> {
                             }
                         }//do nothing
                     }
 
                     override fun onThreeFingerSingleTap() {
-                        setResultTextView("Three Finger Tap")
+                        //setResultTextView("Three Finger Tap")
                     }
 
                     override fun onTwoFingerSingleTap() {
-                        setResultTextView("Two Finger Tap")
+                        //setResultTextView("Two Finger Tap")
                     }
                 })
     }
