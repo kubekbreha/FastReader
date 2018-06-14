253412a7e4de8752da1def69b39bfbc6e1243aa8
Jakub Brehuv <kubek.breha@gmail.com>
2018-06-14 20:50:33 +0200

    add collors

---
+++ app/src/main/java/com/kubekbreha/fastreader/theme/Theme.kt
@@ -0,0 +1,25 @@
+package com.kubekbreha.fastreader.theme
+
+/**
+ * Created by Pankaj on 03-11-2017.
+ */
+
+class Theme {
+    var id: Int = 0
+    var primaryColor: Int = 0
+    var primaryDarkColor: Int = 0
+    var accentColor: Int = 0
+
+    constructor(primaryColor: Int, primaryDarkColor: Int, accentColor: Int) {
+        this.primaryColor = primaryColor
+        this.primaryDarkColor = primaryDarkColor
+        this.accentColor = accentColor
+    }
+
+    constructor(id: Int, primaryColor: Int, primaryDarkColor: Int, accentColor: Int) {
+        this.id = id
+        this.primaryColor = primaryColor
+        this.primaryDarkColor = primaryDarkColor
+        this.accentColor = accentColor
+    }
+}
